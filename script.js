b9c68038d32b450f93a441cd2c7375ed655d7e28
Pgalli992 <p.galli@hotmail.it>
2023-07-03 15:21:29 +0200

    Fourth commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Sticky navigation
+
+const sectionOpereEl = document.querySelector(".gallery__item--5");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionOpereEl);
