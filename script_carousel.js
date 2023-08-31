0b1390f966d16b271d71d48bd09e6b1a1beefe4b
Paolo Galli <paologalli@Mini-di-Paolo.station>
2023-08-31 12:48:42 +0200

    tenth commit

--- script_carousel.js
+++ script_carousel.js
@@ -1,49 +1,89 @@
 "use strict";
 
-const btnLeft = document.querySelector(".btn--left");
-const btnRight = document.querySelector(".btn--right");
+const reviews = document.getElementsByClassName("reviews");
+const articles = document.getElementsByClassName("article");
+const btnLeftR = document.querySelector(".btn--left-r");
+const btnRightR = document.querySelector(".btn--right-r");
+const btnLeftA = document.querySelector(".btn--left-a");
+const btnRightA = document.querySelector(".btn--right-a");
 let pre = 0;
 let rev = 0;
-carousel(rev, pre);
+let prea = 0;
+let art = 0;
+carousel(reviews, rev, pre);
+carousel(articles, art, pre);
 
-// Call the addEventListener method
+// Call the addEventListener method for review section
 ["click", "touch"].forEach((event) => {
-  btnLeft.addEventListener(event, function () {
+  btnLeftR.addEventListener(event, function () {
+    reviews;
     pre = rev;
     rev -= 1;
-    console.log(rev);
-    console.log(pre);
-    carousel(rev, pre);
+    carousel(reviews, rev, pre);
   });
 });
 
 ["click", "touch"].forEach((event) => {
-  btnRight.addEventListener(event, function () {
+  btnRightR.addEventListener(event, function () {
+    reviews;
     pre = rev;
     rev += 1;
-    console.log(rev);
-    console.log(pre);
-    carousel(rev, pre);
+    carousel(reviews, rev, pre);
   });
 });
 
-function carousel(review, preav) {
-  let reviews = document.getElementsByClassName("reviews");
-  console.log(reviews);
-  if (review >= reviews.length) {
+// Call the addEventListener method for review section
+["click", "touch"].forEach((event) => {
+  btnLeftA.addEventListener(event, function () {
+    articles;
+    prea = art;
+    art -= 1;
+    carousel(articles, art, prea);
+  });
+});
+
+["click", "touch"].forEach((event) => {
+  btnRightA.addEventListener(event, function () {
+    articles;
+    prea = art;
+    art += 1;
+    carousel(articles, art, prea);
+  });
+});
+
+function greaterOrEqual() {
+  let review = 0;
+  let preav = type.length - 1;
+}
+
+function lessthan() {
+  let preav = 0;
+}
+
+function carousel(type, review, preav) {
+  type;
+  if (type === reviews && review >= type.length) {
     review = 0;
-    preav = reviews.length - 1;
+    preav = type.length - 1;
     rev = 0;
-    console.log("+");
   }
-  if (review < 0) {
-    review = reviews.length - 1;
+  if (type === reviews && review < 0) {
+    review = type.length - 1;
     preav = 0;
-    rev = reviews.length - 1;
-    console.log("-");
+    rev = type.length - 1;
   }
-  console.log(preav);
-  reviews[preav].classList.remove("reviews--visible");
-  reviews[review].classList.add("reviews--visible");
-  console.log("c");
+  if (type === articles && review >= type.length) {
+    review = 0;
+    preav = type.length - 1;
+    art = 0;
+  }
+  if (type === articles && review < 0) {
+    review = type.length - 1;
+    preav = 0;
+    art = type.length - 1;
+  }
+  type[preav].classList.remove("visible");
+  type[review].classList.add("visible");
+  // type[review].classlist.toggle("visible");
+  // type[preav].classlist.toggle("visible");
 }
